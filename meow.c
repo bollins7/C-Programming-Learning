8fd14168576581d6ed5f151687098338e7795c59
Chris <37301037+bollins7@users.noreply.github.com>
2023-08-05 13:21:08 -0500

    For and while loops
    loop practice

---
+++ meow.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* Variables & While Loops
+
+int main(void)
+{
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
+}
+*/
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+    printf("meow\n");
+    }
+}
